7bcc4f72ebba5bf6e66dcdd1a47e79b2fa05dfb8
AdrianRosasform43 <81123531+AdrianRosasform43@users.noreply.github.com>
2021-03-22 20:17:34 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,12 +2,12 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyB5gmrYkyaiRhz99u35l0P8JrjnKC4awbs",
-    authDomain: "autenticaraai.firebaseapp.com",
-    projectId: "autenticaraai",
-    storageBucket: "autenticaraai.appspot.com",
-    messagingSenderId: "841100038413",
-    appId: "1:841100038413:web:faad2d52b381b9be18a044",
-    measurementId: "G-K38XREM7Q6"
+   apiKey: "AIzaSyBWfZD9D8mCKEhKlyUsX1xT2CsAx37yzqE",
+    authDomain: "autentificar43raai-e7c6f.firebaseapp.com",
+    projectId: "autentificar43raai-e7c6f",
+    storageBucket: "autentificar43raai-e7c6f.appspot.com",
+    messagingSenderId: "693109106214",
+    appId: "1:693109106214:web:31548956a258bb7d46a8e7",
+    measurementId: "G-R3C3SNF6CB"
     });
 
